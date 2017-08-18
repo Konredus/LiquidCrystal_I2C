140cd1cb927b5b4917e8c861239972d71daa17d8
Konredus <konradpeschka@hotmail.com>
2017-08-18 19:56:01 -0300

    Update RecibeSerial_imprimeLCD.ino

--- examples/RecibeSerial_imprimeLCD/RecibeSerial_imprimeLCD.ino
+++ examples/RecibeSerial_imprimeLCD/RecibeSerial_imprimeLCD.ino
@@ -7,7 +7,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); //configuramos la pantalla con su direccion y el tipo
+LiquidCrystal_I2C lcd(0x3f, 16, 2); //configuramos la pantalla con su direccion y el tipo
 //recordar que para obtener las direccion tenemos que usar el codigo
 //que viene en lso ejemplos de scanner_i2c
 
@@ -16,6 +16,7 @@ void setup()
   lcd.init();        //inicializamos LCD
   lcd.backlight();   //activamos luz backlight
   Serial.begin(9600); //establecemos velocidad de comunicacion del puerto serie
+  lcd.clear();   //borramos todo lo que esta escrito en el LCD
 }
 
 void loop()
